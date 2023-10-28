27d363c66f347a95473c84043b1fcf9031e94844
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-10-28 13:56:29 +0530

    Material UI Menu component

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -1,8 +1,23 @@
-import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from '@mui/material';
-import { CatchingPokemon } from '@mui/icons-material';
+import { AppBar, Toolbar, IconButton, Typography, Stack, Button, Menu, MenuItem } from '@mui/material';
+import { CatchingPokemon, KeyboardArrowDown } from '@mui/icons-material';
+import { useState } from 'react';
 // import catchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 
 function MuiNavbar() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+
+  // handle displaying menu bar on click of resources button
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  // handle closing the menu
+  const handleClose = (_event: any) => {
+    setAnchorEl(null);
+  }
+
   return (
     // Navbar component using AppBar and Toobar
     <AppBar position='static'>
@@ -15,8 +30,41 @@ function MuiNavbar() {
           <Button color='inherit'>Features</Button>
           <Button color='inherit'>Pricing</Button>
           <Button color='inherit'>About</Button>
+          <Button 
+            color='inherit' 
+            id='resources-button' 
+            onClick={handleClick}
+            // props ti control open and close of menu
+            aria-controls={open ? 'resources-menu' : undefined}
+            aria-haspopup='true'
+            aria-expanded={open ? 'true' : undefined}
+            endIcon={<KeyboardArrowDown />}
+          >
+            Resources
+          </Button>
           <Button color='inherit'>Login</Button>
         </Stack>
+        <Menu 
+          id='resources-menu' 
+          open={open} 
+          anchorEl={anchorEl} 
+          MenuListProps={{
+            'aria-labelledby': 'resources-button'
+          }}
+          onClose={handleClose}
+          // props to set position of menu
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right'
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right'
+          }}
+        >
+          <MenuItem>Blog</MenuItem>
+          <MenuItem>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   )
